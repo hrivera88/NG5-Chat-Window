fdc136b75113dd9253d175fc97071ab36d4e81d4
alivehal <hrivera@outlook.com>
2018-07-03 14:31:50 -0500

    Updated Server JS bot directory path

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ const path = require("path");
 const app = express();
 
 // Serve only the static form the dist directory
-app.use(express.static(__dirname + "/dist/node-cub-bot"));
+app.use(express.static(__dirname + "/dist/node-cubBot"));
 
 app.get("/*", function(req, res) {
-  res.sendFile(__dirname + "/dist/node-cub-bot/index.html");
+  res.sendFile(__dirname + "/dist/node-cubBot/index.html");
 });
 
 //Start the app by listening on the default Heroku Port
